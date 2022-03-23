a2fc16da8b5c8b0a4cbf9b176a10eb1ebabb2b74
Benedict Diederich <bene.d@gmx.de>
2022-03-23 11:33:20 -0700

    Updating documentation in main file

--- ESP32/REST_API_JSON_Serial_Wifi_motor_PS3_v0/REST_API_JSON_Serial_Wifi_motor_PS3_v0.ino
+++ ESP32/REST_API_JSON_Serial_Wifi_motor_PS3_v0/REST_API_JSON_Serial_Wifi_motor_PS3_v0.ino
@@ -62,14 +62,15 @@
 
   // trigger camera at a rate of 20hz
 
+  {"task": "/motor_act", "speed0":0, "speed1":0,"speed2":40,"speed3":9000, "isforever":1, "isaccel":1} 
   {"task": "/state_set", "isdebug":0}
   {"task": "/state_act", "delay": 100}
   {
   "task": "multitable",
   "task_n": 2,
-  "repeats_n": 100,
+  "repeats_n": 200,
   "0": {"task": "/digital_act", "digitalid": 1, "digitalval":-1},
-  "1": {"task": "/state_act", "delay": 80}
+  "1": {"task": "/state_act", "delay": 50}
   }
   {"task": "/motor_act", "isstop":1}
   {"task": "/motor_act", "isenable":0}
