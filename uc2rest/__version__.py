fd488425d26de6323cf3dbf88399a0f58bb7fa70
beniroquai <bene.d@gmx.de>
2022-07-02 17:48:45 +0200

    Updating install instructions

--- uc2rest/__version__.py
+++ uc2rest/__version__.py
@@ -6,7 +6,7 @@ __version__.py
 
 __title__ = 'UC2 REST'
 __description__ = 'This pacage will help you to drive the ESP32-driven microscopy control modules from UC2'
-__version__ = '0.1.0c'
+__version__ = '0.1.0d'
 __author__ = 'Benedict Diederich'
 __author_email__ = 'benedictdied@gmail.com'
 __license__ = 'GPL v3'
