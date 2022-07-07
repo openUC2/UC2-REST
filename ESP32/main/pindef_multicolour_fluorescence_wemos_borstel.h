5d1441b9bbc3ad5c4e8b5d2c4181c8b65159541a
beniroquai <bene.d@gmx.de>
2022-07-07 16:28:51 +0200

    modification for laser

--- ESP32/main/pindef_multicolour_fluorescence_wemos_borstel.h
+++ ESP32/main/pindef_multicolour_fluorescence_wemos_borstel.h
@@ -12,7 +12,7 @@
 //#define IS_DAC
 //#define IS_DAC_FAKE
 //#define IS_WIFI
-#define IS_PS4 // ESP32-only
+#define IS_PS3 // ESP32-only
 //#define IS_ANALOG// ESP32-only
 #define IS_LEDARR
 
@@ -22,21 +22,17 @@ int analog_PIN_2 = 0;
 int analog_PIN_3 = 0;
 
 // Laser PWM pins for CNC Shield
-int LASER_PIN_1 = COOLANT_FLOOD_PIN; // was SPINDLEPWMPIN; // Spin Dir
-int LASER_PIN_2 = FEED_HOLD_PIN; // was SPINDLE_ENABLE_PIN;//  Spin En
+int LASER_PIN_1 = SPINDLEPWMPIN; // Spin Dir
+int LASER_PIN_2 = SPINDLE_ENABLE_PIN;//  Spin En
 int LASER_PIN_3 = 0;// 
 
 
-
-
-
-
 // Stepper Motor pins
-int STEP_A = A_STEP_PIN;
+int STEP_A = 0;
 int STEP_X = X_STEP_PIN;
 int STEP_Y = Y_STEP_PIN;
 int STEP_Z = Z_STEP_PIN;
-int DIR_A = A_DIRECTION_PIN;
+int DIR_A = 0;
 int DIR_X = X_DIRECTION_PIN;
 int DIR_Y = Y_DIRECTION_PIN;
 int DIR_Z = Z_DIRECTION_PIN;
@@ -47,11 +43,11 @@ int dac_fake_1 = 0; // RESET-ABORT just toggles between 1 and 0
 int dac_fake_2 = 0; // Coolant
 
 // ledarray
-int LED_ARRAY_PIN =  was FEED_HOLD_PIN; // FEED HOLD 
+int LED_ARRAY_PIN = FEED_HOLD_PIN; // FEED HOLD 
 
 // digital out (e.g. camera trigger)
-int digital_PIN_1 = 0; //  Cycle Start/Resume
-int digital_PIN_2 = 0; //not used/reserved
-int digital_PIN_3 = 0; //not used/reserved
+//int digital_PIN_1 = 0; //  Cycle Start/Resume
+//int digital_PIN_2 = 0; //not used/reserved
+//int digital_PIN_3 = 0; //not used/reserved
 
 const char* identifier_setup = "pindef_multicolour_fluorescence_wemos_borstel";
