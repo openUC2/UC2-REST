b2aa1545feabb5df0cea0be749d8a809e2a70122
beniroquai <bene.d@gmx.de>
2022-08-11 21:48:24 +0200

    Adding new header for freedcam

--- ESP32/main/main.ino
+++ ESP32/main/main.ino
@@ -132,13 +132,15 @@
 //#include "pindef_lightsheet_tomo_PID_espwemos.h" // for the HoLiSheet
 //#include "pindef_xyz_stagescan_ps4.h"
 //#include "pindef_incubator_microscope_zonly_matrix.h" // for the workshop
-#include "pindef_multicolour_fluorescence_wemos_borstel.h" // - for the borstel setup
+//#include "pindef_multicolour_fluorescence_wemos_borstel.h" // - for the borstel setup
 //#include "pindef_lightsheet_espwemos.h" // - for the diagonal lightsheet setup
 //#include "pindef_slm.h" // - for the slm / TFT
 //#include "pindef_cellstorm_wemos.h" // - for the standalone cellstorm setup
 //include "pindef_multicolour_wemos_lena.h"
 //#include "pindef_confocal.h"
 //#include "pindef_oct_eda.h"
+//#include "pindef_polarizationsetup.h"
+#include "pindef_freedcam.h"
 
 
 int DEBUG = 1; // if tihs is set to true, the arduino runs into problems during multiple serial prints..
