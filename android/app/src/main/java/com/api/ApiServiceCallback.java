2ae982e07e173aaf21600220153aa1632c78c983
killerink <killerink@email.de>
2022-09-24 12:28:52 +0200

    remove android app

--- android/app/src/main/java/com/api/ApiServiceCallback.java
+++
@@ -1,7 +0,0 @@
-package com.api;
-
-@FunctionalInterface
-public interface ApiServiceCallback<T> {
-    void onResponse(T response);
-    default void onFailure(Throwable cause) {}
-}
--- android/app/src/main/java/com/uc2control/MyApplication.java
+++
@@ -1,9 +0,0 @@
-package com.uc2control;
-
-import android.app.Application;
-
-import dagger.hilt.android.HiltAndroidApp;
-
-@HiltAndroidApp
-public class MyApplication extends Application {
-}
