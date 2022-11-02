9e21a9475a21c9bed4f189904681b78e97325abf
beniroquai <bene.d@gmx.de>
2022-11-02 23:46:10 +0100

    Update __version__.py

--- uc2rest/__version__.py
+++ uc2rest/__version__.py
@@ -6,7 +6,11 @@ __version__.py
 
 __title__ = 'UC2 REST'
 __description__ = 'This pacage will help you to drive the ESP32-driven microscopy control modules from UC2'
+<<<<<<< Updated upstream
 __version__ = '0.1.0.13'
+=======
+__version__ = 'develop'
+>>>>>>> Stashed changes
 __author__ = 'Benedict Diederich'
 __author_email__ = 'benedictdied@gmail.com'
 __license__ = 'GPL v3'
