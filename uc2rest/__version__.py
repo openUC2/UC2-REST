34f65b7dc9fd7147eecb6b30db5a56db13f2bcc5
beniroquai <bene.d@gmx.de>
2022-11-02 23:09:41 +0100

    Re-Adding version content

---
+++ uc2rest/__version__.py
@@ -0,0 +1,13 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+"""
+
+__title__ = 'UC2 REST'
+__description__ = 'This pacage will help you to drive the ESP32-driven microscopy control modules from UC2'
+__version__ = '0.1.0.13'
+__author__ = 'Benedict Diederich'
+__author_email__ = 'benedictdied@gmail.com'
+__license__ = 'GPL v3'
+__url__ = 'https://github.com/openUC2/UC2-REST'
