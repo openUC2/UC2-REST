832dea3c4785eeb209e92d9ac8fd24c9ce6157cf
Benedict Diederich <bene.d@gmx.de>
2023-02-07 22:22:39 +0100

    Update __version__.py

--- uc2rest/__version__.py
+++ uc2rest/__version__.py
@@ -6,7 +6,7 @@ __version__.py
 
 __title__ = 'UC2 REST'
 __description__ = 'This pacage will help you to drive the ESP32-driven microscopy control modules from UC2'
-__version__ = "develop"
+__version__ = "v0.0.0"
 __author__ = 'Benedict Diederich'
 __author_email__ = 'benedictdied@gmail.com'
 __license__ = 'GPL v3'
